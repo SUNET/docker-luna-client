e83fe52a8a2edbd3d629e6c0da1c4389461d2735
Fredrik Thulin <fredrik@thulin.net>
2017-06-26 09:52:38 +0200

    massive update using pyeleven instead of luna

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,29 @@
-FROM ubuntu:14.04
-MAINTAINER leifj@sunet.se
-RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-RUN apt-get -q update
-RUN apt-get -y upgrade
-COPY luna-client /usr/src/luna-client
-WORKDIR /usr/src/luna-client
-RUN dpkg -i ckdemo_5.4.1-3_amd64.deb cklog_5.4.1-3_amd64.deb cksample_5.4.1-3_amd64.deb configurator_5.4.1-3_amd64.deb htl-client_5.4.1-3_amd64.deb libcryptoki_5.4.1-3_amd64.deb libshim_5.4.1-3_amd64.deb lunacm_5.4.1-3_amd64.deb lunacmu_5.4.1-3_amd64.deb lunadiag_5.4.1-3_amd64.deb lunadpc_1.1.0-5_amd64.deb multitoken_5.4.1-3_amd64.deb salogin_5.4.1-3_amd64.deb uhd_5.4.1-3_amd64.deb vkd_5.4.1-3_amd64.deb vtl_5.4.1-3_amd64.deb
-RUN apt-get -y install opensc git-core python-dev build-essential libz-dev python-pip mercurial swig dnsutils
-RUN pip install --upgrade hg+https://bitbucket.org/PyKCS11/pykcs11#egg=pykcs11
-RUN pip install --upgrade git+git://github.com/leifj/pyeleven.git#egg=pyeleven
-RUN pip install gunicorn flask
+FROM docker.sunet.se/eduid/pythonenv
+
+MAINTAINER eduid-dev <eduid-dev@SEGATE.SUNET.SE>
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && \
+    apt-get -y dist-upgrade && \
+    apt-get install -y \
+      opensc \
+      libz-dev \
+      mercurial \
+      dnsutils \
+      swig \
+      softhsm2 \
+    && apt-get clean
+
+RUN rm -rf /var/lib/apt/lists/*
+
+RUN /opt/eduid/bin/pip install --upgrade hg+https://bitbucket.org/PyKCS11/pykcs11#egg=pykcs11
+RUN /opt/eduid/bin/pip install --upgrade git+git://github.com/leifj/pyeleven.git#egg=pyeleven
+RUN /opt/eduid/bin/pip install gunicorn flask
+
 WORKDIR /
-RUN rm -rf /usr/src/luna-client
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod a+rx /entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
+
+COPY start.sh /start.sh
+RUN chmod 775 /start.sh
+
+CMD ["bash", "/start.sh"]
---
+++ start.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. /opt/eduid/bin/activate
+
+log_dir=${log_dir-'/var/log/eduid'}
+var_dir=${var_dir-'/var/lib/softhsm'}
+logfile=${logfile-"${log_dir}/${eduid_name}.log"}
+p11_module=${p11_module-'/usr/lib/x86_64-linux-gnu/softhsm/libsofthsm2.so'}
+
+PYELEVEN_ARGS=${PYELEVEN_ARGS-'-w2 --user eduid --group softhsm --reload'}
+PYELEVEN_PORT=${PYELEVEN_PORT-'8000'}
+PKCS11PIN=${PKCS11PIN-'1234'}
+PKCS11SOPIN=${PKCS11SOPIN-'123456'}
+
+if [ ! -d "${var_dir}/tokens" ]; then
+    echo "$0: Initializing SoftHSM token"
+    mkdir /var/lib/softhsm/tokens
+    softhsm2-util --init-token --slot 0 --label "py11softhsm" --pin "${PKCS11PIN}" --so-pin "${PKCS11SOPIN}"
+    if [ -f "${p11softhsm_init_rsa_key}" ]; then
+	echo "$0: Loading private RSA key from ${p11softhsm_init_rsa_key}"
+	tmpder="/dev/shm/py11softhsm_privkey.der"
+	touch ${tmpder}
+	chmod 600 ${tmpder}
+	openssl rsa -inform PEM -outform DER -in "${p11softhsm_init_rsa_key}" -out ${tmpder}
+	pkcs11-tool --module "${p11_module}" --slot 0 --id 1 --label eduid --login --pin "${PKCS11PIN}" \
+		    -y privkey -w ${tmpder}
+	rm ${tmpder}
+    fi
+fi
+
+chown -R eduid: "${log_dir}"
+chown -R eduid:softhsm "${var_dir}" /etc/softhsm
+find "${var_dir}" -type d -print0 | xargs -0 chmod 750
+find "${var_dir}" -type f -print0 | xargs -0 chmod 640
+
+#touch "${logfile}"
+#chgrp eduid "${logfile}"
+#chmod 660 "${logfile}"
+
+if [ "x$ENABLE_PKCS11_SPY" != "x" ]; then
+    echo "$0: Enabling pkcs11-spy since \$ENABLE_PKCS11_SPY is set"
+    PKCS11SPY="${p11_module}"
+    p11_module='/usr/lib/x86_64-linux-gnu/pkcs11-spy.so'
+    export PKCS11SPY
+fi
+
+cat>/config.py<<EOF
+DEBUG = True
+PKCS11MODULE = "${p11_module}"
+PKCS11PIN = "${PKCS11PIN}"
+EOF
+
+echo "$0: Starting Gunicorn app pyeleven:app"
+exec gunicorn -b "0.0.0.0:${PYELEVEN_PORT}" ${PYELEVEN_ARGS} pyeleven:app
